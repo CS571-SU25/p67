95e369fe277a95c25ab43d4ada6b6b71b86e31d5
kuko798 <122519798+kuko798@users.noreply.github.com>
2025-08-04 23:31:17 -0500

    Update EventImage.jsx

--- src/componets/EventImage.jsx
+++ src/componets/EventImage.jsx
@@ -1,14 +1,14 @@
 import { Carousel, Image} from "react-bootstrap"
 export default function EventImage({ img, event }) {
   const eventImages = [
-    { id: 1, img: "img\IMG_6077.jpeg", event: "General Body Fall 2024" },
-    { id: 2, img: "img\IMG_6075.jpeg", event: "Game Night" },
-    { id: 3, img: "img\IMG_6076.jpeg", event: "Headshots & Haircuts" },
+    { id: 1, img: "https://github.com/CS571-SU25/p67/blob/main/src/img/IMG_6077.jpeg?raw=true", event: "General Body Fall 2024" },
+    { id: 2, img: "https://github.com/CS571-SU25/p67/blob/main/src/img/IMG_6075.jpeg?raw=true", event: "Game Night" },
+    { id: 3, img: "https://github.com/CS571-SU25/p67/blob/main/src/img/IMG_6076.jpeg?raw=true", event: "Headshots & Haircuts" },
 
-    { id: 4, img: "img\IMG_8742.jpg",  event: "General Body Fall 2024" },
-    { id: 5, img: "img\GN1.JPG", event: "Game Night" },
-    { id: 6, img: "img\IMG_0827.JPG", event: "Game Night" },
-    { id: 7, img: "img\IMG_2841.jpg", event: "Headshots & Haircuts" }
+    { id: 4, img: "https://github.com/CS571-SU25/p67/blob/main/src/img/IMG_8742.jpg?raw=true",  event: "General Body Fall 2024" },
+    { id: 5, img: "https://github.com/CS571-SU25/p67/blob/main/src/img/GN1.JPG?raw=true", event: "Game Night" },
+    { id: 6, img: "https://github.com/CS571-SU25/p67/blob/main/src/img/IMG_0827.JPG?raw=true", event: "Game Night" },
+    { id: 7, img: "https://github.com/CS571-SU25/p67/blob/main/src/img/IMG_2841.jpg?raw=true", event: "Headshots & Haircuts" }
   ];
   return (
     
